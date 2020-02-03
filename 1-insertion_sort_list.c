085fd082054c79ed7c1e9434bc2a2bd172004529
steven-cruz <stevencruz.300008@gmail.com>
2020-02-03 16:37:18 +0000

    insertion

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - C function to insert node via sort
+ * @list: Double pointer to list of struct listint_t type
+ * Return: Return sorted list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *cNode = NULL, *pNode = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	cNode = *list;
+	while (cNode != NULL)
+	{
+		pNode = cNode;
+		while (pNode->prev != NULL && pNode->n < pNode->prev->n)
+		{
+			pNode->prev->next = pNode->next;
+			if (pNode->next != NULL)
+				pNode->next->prev = pNode->prev;
+			pNode->next = pNode->prev;
+			pNode->prev = pNode->prev->prev;
+			pNode->next->prev = pNode;
+			if (pNode->prev == NULL)
+				*list = pNode;
+			else
+				pNode->prev->next = pNode;
+			print_list(*list);
+		}
+		cNode = cNode->next;
+	}
+}
