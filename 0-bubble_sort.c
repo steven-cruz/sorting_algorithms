be4dbd216d17c21034548f714fd66457043321e5
steven-cruz <stevencruz.300008@gmail.com>
2020-02-03 19:25:46 +0000

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,7 +11,7 @@ void bubble_sort(int *array, size_t size)
 	size_t check = size;
 	size_t i, temp = 0;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size < 2)
 		return;
 
 	while (check > 0)
