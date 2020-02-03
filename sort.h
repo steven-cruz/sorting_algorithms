dcd6f5375bb4b2a3bc98e413d9db24eb36ee7e85
steven-cruz <stevencruz.300008@gmail.com>
2020-02-03 16:43:41 +0000

    header file

--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void print_list(const listint_t *list);
 
 
 void bubble_sort(int *array, size_t size); /* task 0 */
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
 #endif
